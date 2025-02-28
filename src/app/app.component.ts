a814e1d7c1b55d8410b0b6943bfc77897526446d
KennyLond <kennyadso15@gmail.com>
2025-02-28 00:53:25 -0500

    Se a comfigurado la tabla y ahora realizaremos la paginación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle'
+import { ListPersonasComponent } from "./components/list-personas/list-personas.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ MatSlideToggleModule, ListPersonasComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+//import de angular material
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import { Persona } from '../../interfaces/persona';
+
+//import de PIPE
+import { CommonModule } from '@angular/common'; // ✅ Importa CommonModule
 @Component({
   selector: 'app-list-personas',
-  imports: [],
+  imports: [MatToolbarModule,MatCardModule, MatTableModule, CommonModule],
   templateUrl: './list-personas.component.html',
   styleUrl: './list-personas.component.css'
 })
 export class ListPersonasComponent {
+  displayedColumns: string[] = ['nombre', 'apellido', 'correo', 'tipoDocumento', 'documento', 'fechaNacimiento'];
+  dataSource = listPersonas;
 
 }
+
+
+const listPersonas: Persona[] = [
+  {nombre:"Kenny", apellido:"Londoño", correo:"kenny@gmail.com", tipoDocumento:"cc", documento:1091675329, fechaNacimiento: new Date()},
+];
+
