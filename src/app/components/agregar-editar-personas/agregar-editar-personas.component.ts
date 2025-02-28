446bcc0126a4c665c4c2c369f9168579f12aaf39
KennyLond <kennyadso15@gmail.com>
2025-02-28 14:30:13 -0500

    Terminamos el Modal y Empezamos a realizar el Formulario

--- src/app/components/agregar-editar-personas/agregar-editar-personas.component.ts
+++ src/app/components/agregar-editar-personas/agregar-editar-personas.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-agregar-editar-personas',
-  imports: [],
+  imports: [MatDialogModule,MatButtonModule ],
   templateUrl: './agregar-editar-personas.component.html',
   styleUrl: './agregar-editar-personas.component.css'
 })
 export class AgregarEditarPersonasComponent {
 
+  constructor(public dialogRef: MatDialogRef<AgregarEditarPersonasComponent>){
+
+  }
+
+cancelar(){
+  this.dialogRef.close();
+}
 }
--- src/app/components/list-personas/list-personas.component.ts
+++ src/app/components/list-personas/list-personas.component.ts
@@ -1,37 +1,47 @@
-// Importaciones de Angular Material
+// Importaciones de Angular y Angular Material
+import { Component, ViewChild, AfterViewInit } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDialog, MatDialogModule } from '@angular/material/dialog';
-
-// Importación de CommonModule
-import { CommonModule } from '@angular/common'; 
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { Persona } from '../../interfaces/persona';
 import { AgregarEditarPersonasComponent } from '../agregar-editar-personas/agregar-editar-personas.component';
+
+// Datos de ejemplo
+const listPersonas: Persona[] = [
+  { nombre: "Kenny", apellido: "Londoño", correo: "kenny@gmail.com", tipoDocumento: "CC", documento: 1091675329, fechaNacimiento: new Date() },
+  { nombre: "Marcela", apellido: "Paez", correo: "marcela@gmail.com", tipoDocumento: "CC", documento: 145675329, fechaNacimiento: new Date() },
+  { nombre: "Karen", apellido: "Suarez", correo: "karen@gmail.com", tipoDocumento: "CC", documento: 104545329, fechaNacimiento: new Date() },
+  { nombre: "Marta", apellido: "Rodríguez", correo: "marta@gmail.com", tipoDocumento: "CC", documento: 1075329, fechaNacimiento: new Date() }
+];
 
 @Component({
   selector: 'app-list-personas',
   standalone: true,
-  imports: [MatToolbarModule, MatCardModule, MatTableModule, CommonModule, MatPaginatorModule, MatSortModule,MatFormFieldModule,MatInputModule,MatIconModule,MatTooltipModule,MatButtonModule,MatDialogModule],
+  imports: [
+    CommonModule, MatToolbarModule, MatCardModule, MatTableModule, 
+    MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule, 
+    MatIconModule, MatTooltipModule, MatButtonModule, MatDialogModule
+  ],
   templateUrl: './list-personas.component.html',
-  styleUrls: ['./list-personas.component.css'] // 🔹 Corregido "styleUrls"
+  styleUrls: ['./list-personas.component.css']
 })
 export class ListPersonasComponent implements AfterViewInit {
   displayedColumns: string[] = ['nombre', 'apellido', 'correo', 'tipoDocumento', 'documento', 'fechaNacimiento', 'acciones'];
   dataSource: MatTableDataSource<Persona>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
-  @ViewChild(MatSort) sort!: MatSort; // 🔹 Declarado correctamente fuera del constructor
+  @ViewChild(MatSort) sort!: MatSort;
 
-  constructor(public dialog: MatDialog) {
+  constructor(public dialog: MatDialog) { // ✅ Cambiado 'dialogRef' por 'dialog'
     this.dataSource = new MatTableDataSource(listPersonas);
   }
 
@@ -44,24 +54,20 @@ export class ListPersonasComponent implements AfterViewInit {
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
-if(this.dataSource.paginator){
-  this.dataSource.paginator.firstPage();
-}
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
   addEditPersona() {
-    const dialogRef = this.dialog.open(AgregarEditarPersonasComponent, {
+    const dialogRef: MatDialogRef<AgregarEditarPersonasComponent> = this.dialog.open(AgregarEditarPersonasComponent, {
       width: '550px',
-      height: '300px'
+      disableClose: true,
     });
 
+    dialogRef.afterClosed().subscribe(result => { 
+      console.log('El diálogo fue cerrado', result);
+    });
   }
 }
 
-// Datos de ejemplo
-const listPersonas: Persona[] = [
-  { nombre: "Kenny", apellido: "Londoño", correo: "kenny@gmail.com", tipoDocumento: "CC", documento: 1091675329, fechaNacimiento: new Date() },
-  { nombre: "Marcela", apellido: "Paez", correo: "marcela@gmail.com", tipoDocumento: "CC", documento: 145675329, fechaNacimiento: new Date() },
-  { nombre: "Karen", apellido: "Suarez", correo: "karen@gmail.com", tipoDocumento: "CC", documento: 104545329, fechaNacimiento: new Date() },
-  { nombre: "Marta", apellido: "Rodríguez", correo: "marta@gmail.com", tipoDocumento: "CC", documento: 1075329, fechaNacimiento: new Date() }
-];
